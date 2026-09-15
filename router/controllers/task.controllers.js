0491072d90c7aedf6d9f4a53811336817a014ade
mdshihab75 <mostakimshihab75@gmail.com>
2026-09-15 23:59:49 +0600

    add

--- router/controllers/task.controllers.js
+++ router/controllers/task.controllers.js
@@ -1,5 +1,8 @@
-let add_task_controller = (req, res) => {
-    res.send('shihab nafisa')
+const usermodel = require("../../model/user.model");
+let add_task_controller = async(req, res) => {
+    let add_user = new usermodel(req.body)
+    await add_user.save()
+    res.status(201).json({success:true, message:"user created", data:add_user})
 }
 
 module.exports = {add_task_controller}
