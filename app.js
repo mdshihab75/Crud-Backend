e5df0069f6427412b567eaa0afff62c3b07d634b
mdshihab75 <mostakimshihab75@gmail.com>
2026-09-16 23:57:45 +0600

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
-const express = require('express')
+const dns = require('dns')
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+const express = require('express');
+const router = require('./router');
+const dbconect = require('./config/db');
 const app = express()
-
+dbconect()
+app.use(express.json())
+app.use('/', router)
 
 
 module.exports = app;
